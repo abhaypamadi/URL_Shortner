6e50974363fa4728bc9996de28b8fd1d1f3dce98
Abhay M Pamadi <70917073+abhaypamadi@users.noreply.github.com>
2022-08-19 22:41:56 +0530

    Update settings.py

--- simple_url_shortner/simple_url_shortner/settings.py
+++ simple_url_shortner/simple_url_shortner/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oh#7%qrfbrhp5errksvc#j6g7rx8!nc(4myqiduq5k7^_$6%k='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
